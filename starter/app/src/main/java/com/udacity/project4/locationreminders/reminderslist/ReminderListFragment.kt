7c795874edf4f4d0ee9d01a74f5c064370e3914f
Infilong <ytxqfe@gmail.com>
2021-11-19 23:18:17 +0800

    ReminderListFragment.kt sign out issue, edited

--- starter/app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -3,19 +3,13 @@ package com.udacity.project4.locationreminders.reminderslist
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.observe
 import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
 import com.udacity.project4.authentication.AuthenticationActivity
-import com.udacity.project4.authentication.LoginViewModel
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentReminderListBinding
-import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import com.udacity.project4.utils.setTitle
 import com.udacity.project4.utils.setup
@@ -24,7 +18,6 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class ReminderListFragment : BaseFragment() {
     //use Koin to retrieve the ViewModel instance
     override val _viewModel: RemindersListViewModel by viewModel()
-    private val viewModel by viewModels<LoginViewModel>()
 
     private lateinit var binding: FragmentReminderListBinding
     override fun onCreateView(
@@ -54,6 +47,7 @@ class ReminderListFragment : BaseFragment() {
         binding.addReminderFAB.setOnClickListener {
             navigateToAddReminder()
         }
+
     }
 
     override fun onResume() {
@@ -82,12 +76,14 @@ class ReminderListFragment : BaseFragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.logout -> {
-//                TODO: add the logout implementation
-                viewModel.authenticationState.observe(AuthenticationActivity(), Observer { authenticationState ->
-                    when (authenticationState) {
-                        LoginViewModel.AuthenticationState.AUTHENTICATED -> AuthUI.getInstance().signOut(requireContext())
+
+                AuthUI.getInstance().signOut(requireContext())
+                    .addOnSuccessListener {
+                        val logOutIntent = Intent(activity, AuthenticationActivity::class.java)
+                        logOutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        startActivity(logOutIntent)
+                        requireActivity().finish()
                     }
-                })
             }
         }
         return super.onOptionsItemSelected(item)
