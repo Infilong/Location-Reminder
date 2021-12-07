8d1f976af7007d74c4cd2963aba6b5a5767c865f
Infilong <ytxqfe@gmail.com>
2021-12-07 21:32:55 +0800

    Fixed Reason: IllegalArgumentException, message: Unsupported class file major version 59.Changed Robolectric to 4.7.3, link: https://github.com/robolectric/robolectric/issues/6521

--- starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,10 +1,16 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.data.FakeDataSource
-
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.core.Is.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,11 +19,32 @@ import org.junit.runner.RunWith
 class SaveReminderViewModelTest {
     //provide testing to the SaveReminderView and its live data objects
     private lateinit var fakeDataSource: FakeDataSource
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setViewModel() {
+        fakeDataSource = FakeDataSource()
+        saveReminderViewModel =
+            SaveReminderViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+    }
+
+    fun getReminder(): ReminderDataItem {
+        return ReminderDataItem(
+            title = "title",
+            description = "description",
+            location = "location",
+            latitude = 100.11,
+            longitude = 90.11
+        )
+    }
 
     @Test
-    fun saveReminder() {
-        val saveReminderViewModel =
-            SaveReminderViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+    fun onClear() {
+        saveReminderViewModel.onClear()
+        assertThat(saveReminderViewModel.reminderTitle.value, `is`(nullValue()))
     }
 }
 
